969ef304cbd120310fcf6d04fc4e58d9ce82b05f
leizleho <ho.leizle@gmail.com>
2019-06-10 22:38:59 -0700

    serve images using express

--- src/modules/offer/find/resolvers.ts
+++ src/modules/offer/find/resolvers.ts
@@ -2,6 +2,10 @@ import { ResolverMap } from '../../../types/graphql-utils';
 import { Offer } from '../../../entity/Offer';
 
 export const resolvers: ResolverMap = {
+  Offer: {
+    pictureUrl: (parent, _, { url }) =>
+      parent.pictureUrl && `${url}/images/${parent.pictureUrl}`
+  },
   Query: {
     findOffers: async () => {
       return Offer.find();
