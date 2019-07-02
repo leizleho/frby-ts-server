8c61b9ba566f8423347d5768150377aff2c9661f
leizleho <ho.leizle@gmail.com>
2019-07-01 22:53:35 -0700

    fix pictureUrl resolver

--- src/modules/offer/find/resolvers.ts
+++ src/modules/offer/find/resolvers.ts
@@ -3,13 +3,21 @@ import { offerCacheKey } from '../../../constants';
 
 export const resolvers: ResolverMap = {
   Offer: {
-    pictureUrl: (parent, _, { url }) =>
-      parent.pictureUrl && `${url}/images/${parent.pictureUrl}`,
+    pictureUrl: (parent, _, { url }) => {
+      if (!parent.pictureUrl) {
+        return parent.pictureUrl;
+      }
+      if (parent.pictureUrl.includes('http')) {
+        return parent.pictureUrl;
+      }
+      return `${url}/images/${parent.pictureUrl}`;
+    },
     owner: ({ userId }, _, { userLoader }) => userLoader.load(userId)
   },
   Query: {
     findOffers: async (_, __, { redis }) => {
       const offers = (await redis.lrange(offerCacheKey, 0, -1)) || [];
+
       return offers.map((offer: string) => JSON.parse(offer));
     }
   }
